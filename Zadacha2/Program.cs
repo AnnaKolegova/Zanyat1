8420cdfe0d9e085793a8e108ca6cd20c06b069d0
Anna <eekolegov@mail.ru>
2022-09-22 21:22:41 +0300

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите первое число");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int num2=Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    Console.WriteLine ($"первое число {num1} больше второго числа {num2}");
+}
+else if (num1<num2)
+{
+    Console.WriteLine ($"второе число {num2} больше первого числа {num1}");
+}
+else
+{
+    Console.WriteLine ($"числа равны");
+}
+
