bf465ef2c53ddea062ef15c0bd6b4d353de94d56
Anna <eekolegov@mail.ru>
2022-09-22 21:56:22 +0300

    Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine ("Введите первое число");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int num2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите третье число");
+int num3=Convert.ToInt32(Console.ReadLine());
+if ((num1>num2) & (num1>num3))
+{
+    Console.WriteLine ($"первое число {num1} больше");
+}
+else if ((num2>num1) & (num2>num3))
+{
+    Console.WriteLine ($"второе число {num2} больше");
+}
+else if ((num3>num1) & (num3>num2))
+{
+    Console.WriteLine ($"третье число {num3} больше");
+}
+else
+{
+    Console.WriteLine ($"числа равны");
+}
+
